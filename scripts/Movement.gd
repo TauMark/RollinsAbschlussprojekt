da11f60562fbd149759263dec33260519b091829
Mark Taubert <taubmar14@k2ac.intern>
2026-02-05 12:29:00 +0100

    extract player scene & add sprite flip

--- scripts/Movement.gd
+++ scripts/Movement.gd
@@ -4,6 +4,11 @@ extends CharacterBody2D
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
+	print(input_direction)
+	if input_direction.x == 1:
+		$Sprite2D.scale.x = -1
+	elif input_direction.x == -1:
+		$Sprite2D.scale.x = 1
 	velocity = input_direction * speed
 
 func _physics_process(delta):
