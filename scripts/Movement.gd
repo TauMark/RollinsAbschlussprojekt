ffcfffe1549b6905d74e4b375821bdab0ee93c0b
Mark Taubert <taubmar14@k2ac.intern>
2026-02-05 13:09:24 +0100

    in progress

--- scripts/Movement.gd
+++ scripts/Movement.gd
@@ -1,16 +1,23 @@
 extends CharacterBody2D
 
 @export var speed = 400
+@onready var sprite = $AnimatedSprite2D
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
-	print(input_direction)
-	if input_direction.x == 1:
-		$Sprite2D.scale.x = -1
-	elif input_direction.x == -1:
-		$Sprite2D.scale.x = 1
 	velocity = input_direction * speed
+	return input_direction
 
 func _physics_process(delta):
-	get_input()
+	var direction = get_input()
 	move_and_slide()
+	handle_animation(direction)
+
+func handle_animation(direction):
+	print(sprite)
+	if direction.x > 0:
+		sprite.scale.x = -1
+		sprite.play("walk")
+	elif direction.x < 0:
+		sprite.scale.x = 1
+		sprite.play("walk")
