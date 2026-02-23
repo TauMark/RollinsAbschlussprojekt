18d6d19ca810f8efeb52613300173626f0fcd950
Maik <schnmai28@schueler.bwv-ac.de>
2026-02-23 08:24:34 +0100

    Main menu (#22)
    * main menu with working buttons
    
    * cleanup
    
    ---------
    
    Co-authored-by: Maik Schneider <schnmai28@k2ac.intern>

---
+++ scripts/main_menu.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_new_game_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Main.tscn") #change later to switch to game scene
+
+
+func _on_load_game_pressed() -> void:
+	print("Load Game Pressed") # Replace with function body.
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
