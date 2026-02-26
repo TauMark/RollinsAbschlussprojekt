04cf44e94828306a72917b819101d7ab36895e42
Maik Schneider <schnmai28@k2ac.intern>
2026-02-26 12:03:07 +0100

    added minimap to top right corner

--- scripts/Door.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-@export var room_scene = "res://Scenes/Main.tscn"
-
-
-
-func _on_body_entered(body: Node2D) -> void:
-	if(body.is_in_group('Player')):
-		get_tree().change_scene_to_file(room_scene)
