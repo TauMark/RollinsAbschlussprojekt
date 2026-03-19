f768450e5e2d1d3bc037531e0f434a446feb56e8
Maik Schneider <schnmai28@k2ac.intern>
2026-03-19 12:52:11 +0100

    added end screen and end door to finish game

--- scripts/Map.gd
+++ scripts/Map.gd
@@ -23,3 +23,29 @@ func _on_body_entered_up(body: Node2D) -> void:
 func _on_body_entered_left(body: Node2D) -> void:
 	if(body.is_in_group('Player')):
 		body.position -= Vector2(250,0)
+
+func get_points_from_file() -> int:
+	var path = "res://data/playerdata.json"
+	
+	if not FileAccess.file_exists(path):
+		return 0
+	
+	var file = FileAccess.open(path, FileAccess.READ)
+	var data = JSON.parse_string(file.get_as_text())
+	file.close()
+	
+	if typeof(data) == TYPE_DICTIONARY and data.has("point_counter"):
+		return int(data["point_counter"])
+	
+	return 0
+
+	
+
+
+func _on_dour_end_body_entered(body: Node2D) -> void:
+	var points = get_points_from_file()
+	
+	if points > 9:
+		get_tree().change_scene_to_file("res://Scenes/EndScreen.tscn")
+	else:
+		print("Mindestens 10 Punkte benötigt!")
